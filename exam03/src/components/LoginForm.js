028b2a94dd27ceb21b774bdf2cc506132c298f22
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-14 10:23:31 +0900

    .

---
+++ exam03/src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BigButton as LoginButton } from './commons/StyledButton';
+
+
+const LoginForm = () => {
+     return (
+      <>
+      <h1>로그인</h1>
+      <form>
+        <input type="text" name="userId" placeholder="아이디" />
+        <input type="password" name="password" placeholder="비밀번호" />
+        <LoginButton type="submit">로그인</LoginButton>
+      </form>
+      </>
+     );
+};
+
+export default React.memo(LoginForm);
