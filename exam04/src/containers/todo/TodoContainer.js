120f8a905ea1cc1c1bb286a2134e56bf75e3a36b
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-02-15 10:19:04 +0900

    immer

--- exam04/src/containers/todo/TodoContainer.js
+++ exam04/src/containers/todo/TodoContainer.js
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
+import { produce } from 'immer';
 import TodoForm from '../../components/todo/TodoForm';
 import TodoList from '../../components/todo/TodoList';
 
 const ContentBox = styled.div`
-     box-sizing: border-box;
-     width: 100%;
-     max-width: 720px;
-     margin: 0 auto;
-     background: #f8f8f8;
-     padding: 25px;
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 720px;
+  margin: 0 auto;
+  background: #f8f8f8;
+  padding: 25px;
 
-     *{
-        box-sizing: border-box; 
-     }
+  * {
+    box-sizing: border-box;
+  }
 `;
 
-const todos = [
-        { id: 1, title : "할일1" },
-        { id: 2, title : "할일2" },
-];
-
 const TodoContainer = () => {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      setTodos(
+        produce((draft) => {
+          draft.push({ id: draft.length + 1, title });
+        }),
+      );
+
+      setTitle('');
+    },
+    [title],
+  );
+
+  const onChange = useCallback(
+    (e) => setTitle(() => e.target.value.trim()),
+    [],
+  );
+
+  const onDoubleClick = useCallback(
+    (id) =>
+      setTodos(
+        produce((draft) => {
+          draft.splice(
+            draft.findIndex((todo) => todo.id === id),
+            1,
+          );
+        }),
+      ),
+    [],
+  );
+
   return (
     <ContentBox>
-      <TodoForm />
-      <TodoList todos={todos} />
+      <TodoForm onSubmit={onSubmit} onChange={onChange} title={title} />
+      <TodoList todos={todos} onDoubleClick={onDoubleClick} />
     </ContentBox>
   );
 };
